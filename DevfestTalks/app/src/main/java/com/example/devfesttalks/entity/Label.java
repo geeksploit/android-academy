5493f19ffd9b6117d248d2fc75f47db045594814
Max Rem <max@geeksploit.me>
2019-04-03 16:24:31 +0300

    feat: Define an immutable label entity

---
+++ DevfestTalks/app/src/main/java/com/example/devfesttalks/entity/Label.java
@@ -0,0 +1,15 @@
+package com.example.devfesttalks.entity;
+
+final class Label {
+
+    private final String name;
+
+    Label(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}

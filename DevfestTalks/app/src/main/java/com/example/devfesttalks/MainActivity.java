c205ef9ce801c496db01e5f69870357a23ae32bc
Max Rem <max@geeksploit.me>
2019-04-03 18:33:49 +0300

    feat: Add a simple button to launch the talk detail Activity

--- DevfestTalks/app/src/main/java/com/example/devfesttalks/MainActivity.java
+++ DevfestTalks/app/src/main/java/com/example/devfesttalks/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.devfesttalks;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button showTalkDetails = findViewById(R.id.show_talk_details);
+        showTalkDetails.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TalkDetail.start(MainActivity.this, 0);
+            }
+        });
     }
 }
